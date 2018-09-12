63a2ff086960505fcdc5ba9cd09c307571f6fc9d
phanhue <huethopth@gmail.com>
2018-09-12 12:00:59 +0700

    set view home index

--- src/main/java/com/hue/user/controllers/UserController.java
+++ src/main/java/com/hue/user/controllers/UserController.java
@@ -1,5 +1,18 @@
 package com.hue.user.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//hue.pt
+@Controller
 public class UserController {
+
+    @GetMapping(value = "/")
+    public ModelAndView index(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        return mav;
+    }
 
 }
