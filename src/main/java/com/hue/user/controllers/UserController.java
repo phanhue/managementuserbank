25719687f15d54314abd81fde64994f6a3033e00
phanhue <huethopth@gmail.com>
2018-09-12 15:15:05 +0700

    update login-process session

--- src/main/java/com/hue/user/controllers/UserController.java
+++ src/main/java/com/hue/user/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 //hue.pt
 @Controller
 public class UserController {
@@ -26,8 +28,9 @@ public class UserController {
     }
 
     @PostMapping(value = "/login-process")
-    public ModelAndView loginProcess(UserDTO userDTO){
+    public ModelAndView loginProcess(UserDTO userDTO, HttpSession session){
         System.out.println(userDTO);
+        session.setAttribute("user", userDTO);
         ModelAndView mav = new ModelAndView();
         mav.setViewName("redirect:/");
         return mav;
