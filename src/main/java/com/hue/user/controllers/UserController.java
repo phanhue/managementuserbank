31b34851687b087b682d34f8e81b3bffe0380f4d
phanhue <huethopth@gmail.com>
2018-09-12 13:43:16 +0700

    add Login/login-process

--- src/main/java/com/hue/user/controllers/UserController.java
+++ src/main/java/com/hue/user/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.hue.user.controllers;
 
+import com.hue.user.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 //hue.pt
@@ -14,5 +16,23 @@ public class UserController {
         mav.setViewName("index");
         return mav;
     }
+
+    @GetMapping(value = "/login")
+   public ModelAndView login(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        mav.addObject("user", new UserDTO());
+        return mav;
+    }
+
+    @PostMapping(value = "/login-process")
+    public ModelAndView loginProcess(UserDTO userDTO){
+        System.out.println(userDTO);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("redirect:/");
+        return mav;
+    }
+
+
 
 }
